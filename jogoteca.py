4e05ff24f78314a4c0530ad77de3ec332bac7346
Danilo Andrade <dandrade0803@gmail.com>
2024-04-10 13:22:56 -0300

    Final Aula 05 Autenticando usuários com session

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 
 
 class Jogo:
@@ -15,19 +15,22 @@ jogo3 = Jogo('Tomb Raider', 'Ação-Aventura', 'PS4')
 lista = [jogo1, jogo2, jogo3]
     
 app = Flask(__name__) # Cria uma instância da classe Flask
+app.secret_key = 'dragonballz'
+app.config['SESSION_COOKIE_SAMESITE'] = 'None'
+app.config['SESSION_COOKIE_SECURE'] = True
 
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template('index.html', titulo="Jogos", jogos=lista)
+    return render_template('index.html', titulo='Jogos', jogos=lista)
 
 
-@app.route("/cadastro")
+@app.route('/cadastro')
 def cadastro():
-    return render_template('cadastro.html', titulo="Jogos")
+    return render_template('cadastro.html', titulo='Jogos')
 
 
-@app.route("/criar", methods=['POST',])
+@app.route('/criar', methods=['POST',])
 def criar():
     nome = request. form['nome']
     categoria = request. form['categoria']
@@ -38,6 +41,30 @@ def criar():
     
     return redirect('/')
 
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if request.form['senha'] == 'goku':
+        session['usuario_logado'] = request.form['usuario']
+        flash(f'{session['usuario_logado']} logado com sucesso.')
+        return redirect('/')
+    else:
+        flash('Dados incorretos! Tente novamente.')
+        return redirect('/login')
+
+@app.route('/logout')
+def logout():
+    if session['usuario_logado'] == None:
+        flash('Nenhum usuário está logado')
+        return redirect('/')
+    else:
+        session['usuario_logado'] = None
+        flash('Logout efetuado com sucesso')
+        return redirect('/')
 
 # app.run() # Localhost
 app.run(debug=True) # Pode ser acessado por pessoas conectadas nessa rede
